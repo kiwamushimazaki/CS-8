7b048d42406aa2b2181d588baa5f9650ffe9e171
kiwamu <kiwamu@KIWAMU-PC>
2016-07-07 10:09:17 +0900

    演習８の解答です。

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class Auto
     {
-        static void Main(string[] args)
+        private int displacement;
+        protected double fuel;
+
+        public Auto(int d)
         {
+            displacement = d;
+        }
+        public void AddFuel(double f)
+        {
+            fuel = f;
+        }
+        public int GetDisplacement()
+        {
+            return displacement;
+        }
+    }
+
+    class Sedan : Auto
+    {
+        public Sedan(int d):base(d)
+        {
+
+        }
+    }
+
+    class Truck : Auto
+    {
+        public Truck(int d) : base(d)
+        {
+
+        }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            Sedan auto1 = new Sedan(2000);
+            auto1.AddFuel(50);
+            Console.WriteLine("排気量{0}ccの車", auto1.GetDisplacement());
+
+            Truck auto2 = new Truck(5500);
+            auto2.AddFuel(50);
+            Console.WriteLine("排気量{0}ccの車", auto2.GetDisplacement());
         }
     }
 }
